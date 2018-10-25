f0835f4fcc5b4444af345ec6200669128d5ded31
neverclear86 <neverclear86sab@gmail.com>
2018-10-26 00:20:56 +0900

    first commit

---
+++ src/main/kotlin/pro/ikili/vtuberescape/VTuberEscape.kt
@@ -0,0 +1,15 @@
+package pro.ikili.vtuberescape
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class VTuberEscape : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+        this.logger.info("Lina Tsukusu's Plugin!!!!!!!!!!!!!!!!")
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
